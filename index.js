1b481ea8af5e27dd92c9b64b4cf52d4fb2374d8d
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-13 17:11:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+var newHeader = document.createElement("h1");
+newHeader.innerHTML = "Jana is the champion";
+document.body.appendChild(newHeader);  
+newHeader.setAttribute("id","victory");
