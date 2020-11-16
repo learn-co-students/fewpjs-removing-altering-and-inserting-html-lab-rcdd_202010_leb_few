b24260b46535c3e6042faecc667b377ee737ac8a
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-16 11:25:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+
+ let main=document.querySelector("#main");
+ main.remove();
+ 
+
+ let newHeader= document.createElement("h1");
+ newHeader.id="victory";
+ document.body.appendChild(newHeader);
+ let element=document.querySelector("h1#victory");
+ element.innerHTML= 'Jolie is the champion';
+ 
+
