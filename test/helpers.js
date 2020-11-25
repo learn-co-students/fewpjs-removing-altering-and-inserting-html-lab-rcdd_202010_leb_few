b0658994d6a8ee0826c759ecc15010f07430b18c
Devavrata Tripathy <67550706+Devavrata-Tripathy@users.noreply.github.com>
2020-11-25 16:11:11 +0530

    Update helpers.js
    Done

--- test/helpers.js
+++ test/helpers.js
@@ -18,3 +18,9 @@ const src = babelResult.code
 jsdom({
   html, src
 });
+
+document.querySelector("main#main").remove();
+let newHeader = document.createElement("h1")
+document.body.appendChild(newHeader);
+newHeader.id="victory";
+newHeader.innerHTML="YOUR-NAME is the champion"; 
