3ac43d3786cc364ff486a570a2d9808ed66881a7
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2020-12-05 13:30:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let main = document.getElementById("main");
+main.remove();
+var btn = document.createElement("h1");
+btn.id="victory";
+  document.body.appendChild(btn);
+let newHeader = document.getElementById("victory");
+newHeader.innerHTML ="YOUR-NAME is the champion";
